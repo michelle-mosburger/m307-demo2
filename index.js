0f62fb443867033dd43694f59dbb5b3cd8a5f6ba
Michelle <147482600+michelle-mosburger@users.noreply.github.com>
2024-10-28 10:53:42 +0000

    css

--- index.js
+++ index.js
@@ -1,20 +1,54 @@
-import { createApp } from "./config.js";
+import { createApp, upload } from "./config.js";
 
 const app = createApp({
-  user: "autumn_star_7622",
-  host: "168.119.168.41",
-  database: "demo",
-  password: "uaioysdfjoysfdf",
-  port: 18324,
+  user: "funny_monkey_1284",
+  host: "bbz.cloud",
+  database: "funny_monkey_1284",
+  password: "1237ed3614d24ef81c834e37c3748123",
+  port: 30211,
 });
 
 /* Startseite */
-app.get("/", async function (req, res) {
+/*app.get("/", async function (req, res) {
   res.render("start", {});
+});*/
+
+// Datenbankabfrage event
+
+app.get("/", async function (req, res) {
+  const event = await app.locals.pool.query("select * from event");
+  console.log(event.rows);
+  res.render("start", { event: event.rows });
 });
+
+// Datenbankabfrage user
+
+/*app.get("/", async function (req, res) {
+  const user = await app.locals.pool.query("select * from user");
+  res.render("start", {});
+});/*
+
+
+
+/*Seite Impressum*/
 
 app.get("/impressum", async function (req, res) {
   res.render("impressum", {});
+});
+
+/*Seite Event erstellen*/
+/* Formular Event erstellen */
+app.get("/form", async function (req, res) {
+  res.render("form", {});
+});
+
+/*Event-Formular übermitteln*/
+app.post("/creat_event", upload.single('image') async function (req, res) {
+  await app.locals.pool.query(
+    "INSERT INTO event (event_name, description, place, date) VALUES ($1, $2, $3, $4)",
+    [req.body.title, req.body.description, req.body.place, req.body.date]
+  );
+  res.redirect("/");
 });
 
 /* Wichtig! Diese Zeilen müssen immer am Schluss der Website stehen! */
